7779c769d10805d53f9d5d704b1e0514b4bdeafb
Andrew Taylor <agrahamt@gmail.com>
2017-03-23 13:32:56 -0500

    reuploaded loopsandfizzbuzz.js, completed part 4

--- loopsandfizzbuzz.js
+++ loopsandfizzbuzz.js
@@ -85,13 +85,32 @@ console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Part 4
 // Write a function the returns a FizzBuzz string for some number N (counting up from 1).
+//A fizzbuzz string is defined as the following:
+//For every number from 1 to the input number...
 // - for every number that isn't a multiple of 3 or 5, return a period '.'
 // - for every number that is a multiple of 3 (but not 5), return 'fizz'
 // - for every number that is a multiple of 5 (but not 3), return 'buzz'
 // - for every number that is a multiple of 3 and 5, return 'fizzbuzz'
 
 // TODO: Your function goes here.
-
+function fizzbuzz (num) {
+  var output = ''
+  for (var i = 1; i <= num; i++) {
+    if (((i % 3) !== 0) && ((i % 5) !== 0)) {
+      output = output + '.'
+    }
+    else if (((i % 3) === 0) && ((i % 5) !== 0)) {
+      output = output + 'fizz'
+    }
+    else if (((i % 3) !== 0) && ((i % 5) === 0)) {
+      output = output + 'buzz'
+    }
+    else if (((i % 3) === 0) && ((i % 5) === 0)) {
+      output = output + 'fizzbuzz'
+    }
+  }
+  return output
+}
 console.assert(fizzbuzz(1) === '.')
 console.assert(fizzbuzz(2) === '..')
 console.assert(fizzbuzz(3) === '..fizz')
